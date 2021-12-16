6747e11428b0df2c3423a8dc7be4cbc732545032
ASADMUKHTAR <asadmukhtar28@gmail.com>
2021-12-17 03:03:14 +0500

    Create ArticlesRepository.kt and write a suspend function to fetchArticles,  call fetchArticles and emit appropriate emit results.
     Collect the emitted results from ArticlesRepository.kt to ArticlesViewModel.kt.

---
+++ app/src/main/java/com/asad/nytimes/ui/articles/ArticlesRepository.kt
@@ -0,0 +1,25 @@
+package com.asad.nytimes.ui.articles
+
+import com.asad.nytimes.data.remote.ApiServices
+import com.asad.nytimes.models.ArticleResponse
+import com.asad.nytimes.models.base.State
+import com.asad.nytimes.ui.base.BaseRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class ArticlesRepository @Inject constructor(val apiServices: ApiServices) : BaseRepository() {
+
+    suspend fun fetchArticles(pageId: Int): Flow<State<ArticleResponse>> {
+        return flow {
+            emit(State.Loading())
+            val result = makeApiCall { apiServices.getArticles("$pageId.json") }
+            emit(result)
+        }.flowOn(Dispatchers.IO).catch {
+            emit(State.Error(State.ErrorMessages.SomethingWentWrong()))
+        }
+    }
+}
--- app/src/main/java/com/asad/nytimes/ui/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/asad/nytimes/ui/articles/ArticlesViewModel.kt
@@ -1,8 +1,30 @@
 package com.asad.nytimes.ui.articles
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.asad.nytimes.models.ArticleResponse
+import com.asad.nytimes.models.base.State
+import com.asad.nytimes.ui.base.BaseViewModel
+import com.asad.nytimes.utils.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ArticlesViewModel @Inject constructor() : ViewModel()
+class ArticlesViewModel @Inject constructor(val repository: ArticlesRepository) : BaseViewModel() {
+
+    var apiResponse: MutableSharedFlow<State<ArticleResponse>> = MutableSharedFlow()
+    private val _fetchMoviesApiResponse = SingleLiveEvent<State<ArticleResponse>>()
+    val fetchMoviesApiResponse: LiveData<State<ArticleResponse>> = _fetchMoviesApiResponse
+
+    fun fetchArticles() {
+        viewModelScope.launch {
+            repository.fetchArticles(1).collect {
+                _fetchMoviesApiResponse.value = it
+            }
+        }
+    }
+}
