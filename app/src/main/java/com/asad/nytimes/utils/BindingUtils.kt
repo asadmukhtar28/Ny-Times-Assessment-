064e2124d5fb94b82850be82238cec0170d01941
ASADMUKHTAR <asadmukhtar28@gmail.com>
2021-12-17 15:38:19 +0500

    Set the default placeHolder of article at BindingUtils.kt. Add ic_article.png, ic_calendar.xml icons.

--- app/src/main/java/com/asad/nytimes/utils/BindingUtils.kt
+++ app/src/main/java/com/asad/nytimes/utils/BindingUtils.kt
@@ -3,7 +3,10 @@ package com.asad.nytimes.utils
 import android.text.TextUtils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.asad.nytimes.R
+import com.asad.nytimes.models.Article
+import com.asad.nytimes.ui.articles.adapter.ArticleAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
@@ -14,7 +17,7 @@ object BindingUtils {
     fun setImageUrl(
         imageView: ImageView,
         imageUrl: String,
-        placeHolder: Int = R.drawable.ic_launcher_background
+        placeHolder: Int = R.drawable.ic_article
     ) {
         if (!TextUtils.isEmpty(imageUrl)) {
             val requestOptions = RequestOptions().placeholder(placeHolder).error(placeHolder)
@@ -22,6 +25,15 @@ object BindingUtils {
                 .load(imageUrl)
                 .apply(requestOptions)
                 .into(imageView)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("setArticleAdapter")
+    fun setArticleAdapter(recyclerView: RecyclerView, articles: List<Article> ?) {
+        if (recyclerView.adapter != null && articles !=null) {
+            val adapter = recyclerView.adapter as ArticleAdapter
+            adapter.articleList = articles
         }
     }
 }
