b55242bbae6e2dee2b5c336ec411c2856fab6023
ASADMUKHTAR <asadmukhtar28@gmail.com>
2021-12-17 18:10:01 +0500

    Move appropriate message of error to BaseFragment.kt instead of handling the logic at every Fragment.

--- app/src/main/java/com/asad/nytimes/ui/articles/ArticlesFragment.kt
+++ app/src/main/java/com/asad/nytimes/ui/articles/ArticlesFragment.kt
@@ -1,12 +1,10 @@
 package com.asad.nytimes.ui.articles
 
-import android.text.TextUtils
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.asad.nytimes.BR
 import com.asad.nytimes.R
 import com.asad.nytimes.databinding.FragmentArticlesBinding
-import com.asad.nytimes.models.base.State
 import com.asad.nytimes.ui.articles.adapter.ArticleAdapter
 import com.asad.nytimes.ui.base.BaseFragment
 import com.google.android.material.snackbar.Snackbar
@@ -39,17 +37,7 @@ class ArticlesFragment :
 
     private fun setUpObserver() {
         viewModel.error.observe(viewLifecycleOwner, {
-            val errorMessage = when (it) {
-                is State.ResponseError.SomethingWentWrong -> {
-                    if (!TextUtils.isEmpty(it.message))
-                        it.message
-                    else getString(R.string.something_went_wrong)
-                }
-                is State.ResponseError.InternetConnectionResponseError -> {
-                    getString(R.string.internet_error)
-                }
-            }
-
+            val errorMessage = setAppropriateErrorMessage(it)
             Snackbar.make(viewBinding.root, errorMessage, Snackbar.LENGTH_LONG)
                 .setAction("Ok") {}
                 .show()
--- app/src/main/java/com/asad/nytimes/ui/base/BaseFragment.kt
+++ app/src/main/java/com/asad/nytimes/ui/base/BaseFragment.kt
@@ -2,6 +2,7 @@ package com.asad.nytimes.ui.base
 
 import android.content.Context
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import com.asad.nytimes.R
+import com.asad.nytimes.models.base.State
 
 abstract class BaseFragment<VM : ViewModel, VB : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
     Fragment() {
@@ -49,4 +52,16 @@ abstract class BaseFragment<VM : ViewModel, VB : ViewDataBinding>(@LayoutRes pri
         initUi()
     }
 
+    fun setAppropriateErrorMessage(responseError: State.ResponseError): String {
+        return when (responseError) {
+            is State.ResponseError.SomethingWentWrong -> {
+                if (!TextUtils.isEmpty(responseError.message))
+                    responseError.message
+                else getString(R.string.something_went_wrong)
+            }
+            is State.ResponseError.InternetConnectionResponseError -> {
+                getString(R.string.internet_error)
+            }
+        }
+    }
 }
