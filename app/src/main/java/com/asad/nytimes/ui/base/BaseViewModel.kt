734fda0b577b85bcc0202c940d45ae73971f8bd2
ASADMUKHTAR <asadmukhtar28@gmail.com>
2021-12-17 18:10:50 +0500

    Add SingleLiveEvent.kt class to handle the error, isLoading state to observe only once.

--- app/src/main/java/com/asad/nytimes/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/asad/nytimes/ui/base/BaseViewModel.kt
@@ -1,10 +1,10 @@
 package com.asad.nytimes.ui.base
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.asad.nytimes.models.base.State
+import com.asad.nytimes.utils.SingleLiveEvent
 
 open class BaseViewModel : ViewModel() {
-    val isLoading = MutableLiveData(false)
-    val error = MutableLiveData<State.ResponseError>()
+    val isLoading = SingleLiveEvent<Boolean>()
+    val error = SingleLiveEvent<State.ResponseError>()
 }
